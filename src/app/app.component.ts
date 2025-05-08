53aa011f29e50f63e9f70fc3621b17377581ad17
i.alzumor <i.alzumor@bisan.com>
2025-05-08 15:23:22 +0300

    added the navbar with its functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,16 @@
 import {Component, OnInit} from '@angular/core';
-import { Router, RouterOutlet } from '@angular/router';
+import {Router, RouterOutlet} from '@angular/router';
+import {NavbarComponent} from './navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent implements OnInit {
-  constructor(private router: Router) { }
+  constructor(private router: Router) {
+  }
 
   ngOnInit(): void {
 
