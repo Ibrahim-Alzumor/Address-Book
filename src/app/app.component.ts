6dccb07e380ac5dc122c2463275cca07686fe70d
i.alzumor <i.alzumor@bisan.com>
2025-05-08 12:06:41 +0300

    This is the basics + routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component, OnInit} from '@angular/core';
+import { Router, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,6 +7,10 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'Address-Book';
+export class AppComponent implements OnInit {
+  constructor(private router: Router) { }
+
+  ngOnInit(): void {
+
+  }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import {HomeComponent} from './Contacts/home/home.component';
+import {ListComponent} from './Contacts/list/list.component';
+import {AddComponent} from './Contacts/add/add.component';
+import {LoginComponent} from './Contacts/login/login.component';
 
-export const routes: Routes = [];
+
+
+export const routes: Routes = [
+    {path: '' , component: HomeComponent,},
+    {path: 'list', component: ListComponent,},
+    {path: 'add', component: AddComponent,},
+    {path: 'login', component: LoginComponent,}
+];
