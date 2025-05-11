d49fc26a0496b56a19bf642bff1e43b16363c5d3
i.alzumor <i.alzumor@bisan.com>
2025-05-11 11:04:16 +0300

    finsihed the login page where it pulls from the stored users in local storage and checks if autharised log in it allows the user to go to home

--- src/app/Contacts/login/login.component.ts
+++ src/app/Contacts/login/login.component.ts
@@ -1,17 +1,58 @@
 import {Component} from '@angular/core';
-import {RouterLink, RouterLinkActive} from '@angular/router';
-import {FormsModule} from '@angular/forms';
+import {Router} from '@angular/router';
+import {FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators} from '@angular/forms';
+import {UserService} from '../../Services/user.service';
+import {NgClass} from '@angular/common';
 
 @Component({
   selector: 'app-login',
   imports: [
-    RouterLink,
     FormsModule,
-    RouterLinkActive
+    ReactiveFormsModule,
+    NgClass
   ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  userForm: FormGroup;
+
+  constructor(private fb: FormBuilder, private userService: UserService, private router: Router) {
+    this.userForm = this.fb.group({
+      email: [
+        '',
+        [
+          Validators.required,
+          Validators.email,
+          Validators.pattern(/^[^\s@]+@[^\s@]+\.[^\s@]+$/),
+        ],
+      ],
+      password: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(8),
+        ],
+      ],
+    })
+  }
+
+  loginUser(): void {
+    if (this.userForm.valid) {
+      const formValue = this.userForm.value;
+
+      const Users = JSON.parse(localStorage.getItem('users') || '[]');
+
+
+      const usersFind = Users.find((user: any) =>
+        user.email === formValue.email && user.password === formValue.password
+      );
+
+      if (usersFind) {
+        localStorage.setItem('users', JSON.stringify(usersFind));
+        this.router.navigate(['/']);
+      }
+    }
+  }
 
 }
